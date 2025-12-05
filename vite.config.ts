7a61a4f410326ff00f49bb970dd5b0c60011f356
anilcangueler <anilcan_guler@hotmail.com>
2025-12-05 15:30:54 +0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+```typescript
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+})
+```
+
+3. Dosya: `index.html`
+GitHub'da yeni dosya oluşturup adını `index.html` koyun:
+```html
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Contentivity</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/main.tsx"></script>
+  </body>
+</html>
+```
